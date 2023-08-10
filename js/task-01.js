fff0c9d793b70d72c8754f786b2a86289f92987e
Andrii Mykhailytskyi <andrejmihajlickij43@gmail.com>
2023-08-10 13:56:20 +0300

    task1

--- js/task-01.js
+++ js/task-01.js
@@ -5,7 +5,7 @@ console.log(`Number of categories: ${categoryItems.length}`);
 
 categoryItems.forEach(item => {
   const categoryName = item.querySelector('h2').textContent;
-  const categoryElements = item.querySelectorAll('li').length;
+//   const categoryElements = item.querySelectorAll('li').length;
   
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryElements}`);
